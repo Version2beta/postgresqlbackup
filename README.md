e66811bf5692bec7e412be4fd79c5cfaa045f33e
Rob Martin <rob@version2beta.com>
2013-07-24 17:48:34 -0400

    Minor doc change

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# postgresql_backup
+# postgresqlbackup
 
 This project provides a Python package for managing rotating Postgres backups by database to the local filesystem and to a specified AWS S3 bucket.
 
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'postgresqlbackup',
   url = 'https://github.com/Version2beta/postgresqlbackup',
-  version = '1.0.4',
+  version = '1.0.5',
   author = 'Rob Martin',
   author_email = 'rob@version2beta.com',
   packages = ['postgresqlbackup'],
